387a03b4f9b1246f0ea4901bcd446b68af11cc72
Chan Wei Xian <weixianchan509@yahoo.com>
2025-04-22 23:36:05 +0800

    [version/1.0] Responsive Design with Angular CDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkillComponent } from "./skill/skill.component";
+import { BannerComponent } from "./banner/banner.component";
+import { PersonalInfoComponent } from "./personal-info/personal-info.component";
+import { EducationComponent } from "./education/education.component";
+import { WorkExpComponent } from "./work-exp/work-exp.component";
+import { ProjectComponent } from "./project/project.component";
+import { ContactComponent } from "./contact/contact.component";
+import { LayoutModule } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SkillComponent, BannerComponent, PersonalInfoComponent, EducationComponent, 
+    WorkExpComponent, ProjectComponent, ContactComponent,
+    LayoutModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

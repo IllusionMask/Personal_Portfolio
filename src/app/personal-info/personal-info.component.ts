d32e80eef16315b4ec89d62404c908e731c8e4d6
Chan Wei Xian <weixianchan509@yahoo.com>
2025-05-02 22:45:19 +0800

    [version/1.0] Personal Info Component

--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personal-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './personal-info.component.html',
   styleUrl: './personal-info.component.css'
 })
 export class PersonalInfoComponent {
 
+    myData: string[][] = [
+      ['Name', 'John Doe'],
+      ['DOB', '10/10/2025'],
+      ['Work Exp', '2 Years'],
+      ['Education', 'University'],
+      ['Interests', 'Coding']
+    ];
+
+    aboutMe: string[] = ['Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+      'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'];
+
+    constructor() {}
+
+    ngOnInit() : void {}
 }

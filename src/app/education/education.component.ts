141594e86f668ade50bca2098863fd48f51d87bd
Chan Wei Xian <weixianchan509@yahoo.com>
2025-05-02 23:14:18 +0800

    [version/1.0] Education Component

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { Education } from '../models/models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
 
+  educationList: Education[] = [
+    {
+      institute: "High School",
+      course: 'Science',
+      duration: '2012-2017',
+      score: '70%',
+    },
+    {
+      institute: "University",
+      course: 'Foundation',
+      duration: '2018-2018',
+      score: '90%',
+    },
+    {
+      institute: "University",
+      course: 'Degree',
+      duration: '2019-2021',
+      score: '80%',
+    },
+    {
+      institute: "University",
+      course: 'Degree',
+      duration: '2019-2021',
+      score: '80%',
+    }
+  ];
+
+  constructor() {};
+
+  ngOnInit() :void {}
 }
